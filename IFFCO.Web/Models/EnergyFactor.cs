01460d6fbaaf5606ceb9316357cde4e02e9a50dc
Sumit Kumar <imsumitsingh_@outlook.com>
2022-03-02 14:14:23 +0530

    energy factor

---
+++ IFFCO.Web/Models/EnergyFactor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IFFCO.TECHPROD.Web.Models
+{
+    public class EnergyFactor
+    {
+        public string FromDate { get; set; }
+        public string ToDate { get; set; }
+        public string PrCode { get; set; }
+        public string EFFUnit { get; set; }   
+        public double PrValue { get; set; }
+      
+    }
+}
