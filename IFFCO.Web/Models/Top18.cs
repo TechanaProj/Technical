33f4840cdebce83104268c203ee1faefa247b989
RAHUL SHEKHAR <rahulshekhar@iffco.in>
2022-03-07 14:23:41 +0530

    Commit

---
+++ IFFCO.Web/Models/Top18.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IFFCO.TECHPROD.Web.Models
+{
+    public class Top18
+    {
+        public string FN_YR { get; set; }
+        public int S_NO { get; set; }
+        public string CATALYST { get; set; }
+        public string UNITS { get; set; }
+        public string PLANT_UNIT { get; set; }
+        public double? MAY { get; set; }
+        public double? APR { get; set; }
+        public double? JUN { get; set; }
+        public double? JUL { get; set; }
+        public double? AUG { get; set; }
+        public double? SEP { get; set; }
+        public double? OCT { get; set; }
+        public double? NOV { get; set; }
+        public double? DEC { get; set; }
+        public double? JAN { get; set; }
+        public double? FEB { get; set; }
+        public double? MAR { get; set; }
+
+
+
+    }
+}
