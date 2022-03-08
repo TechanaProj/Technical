39b0283ffe776e55dbfeb8eee32f90fb4e37163f
RAHUL SHEKHAR <rahulshekhar@iffco.in>
2022-03-08 12:01:30 +0530

    commit

--- IFFCO.Web/Areas/M1/Controllers/TOP18Controller.cs
+++ IFFCO.Web/Areas/M1/Controllers/TOP18Controller.cs
@@ -60,25 +60,82 @@ namespace IFFCO.TECHPROD.Web.Areas.M1.Controllers
 
 
 
-        public IActionResult Save(string Sno, string Plant_cat, string Uom, string Pno, string APR, string MAY, string JUN, string JUL, string AUG, string SEP, string OCT, string NOV, string DEC, string JAN, string FEB, string MAR, string finyear, string plant)
+        public IActionResult Save(string op,string Sno, string Plant_cat, string Uom, string Pno, string APR, string MAY, string JUN, string JUL, string AUG, string SEP, string OCT, string NOV, string DEC, string JAN, string FEB, string MAR, string finyear, string plant)
         {
             int EMP_ID = Convert.ToInt32(HttpContext.Session.GetInt32("EmpID"));
 
             string moduleid = Convert.ToString(HttpContext.Session.GetString("ModuleID"));
             string controller = this.ControllerContext.RouteData.Values["controller"].ToString();
+            //try
+            //{
+            //    if (op == "update")
+            //    {
+            //        int i = TechnicalCommonService.UpdateRecordsTOP18(Sno, Plant_cat, Uom, Pno, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC, JAN, FEB, MAR, finyear, plant);
+            //        if (i > 0)
+            //        {
+            //            List<TOP3Data> data = TechnicalCommonService.GetRecordsTOP3(finyear, plant);
+            //            ViewBag.records = data;
+            //        }
+            //        else if (i == -1)
+            //        {
+            //            CommonViewModel.errorMessage = "Sno,Fin Year and Plant must be unique";
+            //            return Json(CommonViewModel);
+            //        }
+            //    }
+            //    int i = TechnicalCommonService.SaveRecordsTOP18(Sno, Plant_cat, Uom, Pno, APR, MAY,  JUN, JUL, AUG, SEP, OCT, NOV, DEC, JAN, FEB, MAR, finyear, plant);
+            //    if (i > 0)
+            //    {
+            //        List<TOP18Data> data = TechnicalCommonService.GetRecordsTOP18(finyear, plant);
+            //        ViewBag.records = data;
+            //    }
+            //    else if (i == -1)
+            //    {
+            //        CommonViewModel.errorMessage = "Personel No already addedd";
+            //        return Json(CommonViewModel);
+            //    }
+
+            //}
+            //catch (Exception ex)
+            //{
+
+            //    commonException.GetCommonExcepton(CommonViewModel, ex);
+            //    CommonViewModel.AreaName = this.ControllerContext.RouteData.Values["area"].ToString();
+            //    CommonViewModel.SelectedMenu = this.ControllerContext.RouteData.Values["controller"].ToString();
+            //    return Json(CommonViewModel);
+
+            //}
+
             try
             {
 
-                int i = TechnicalCommonService.SaveRecordsTOP18(Sno, Plant_cat, Uom, Pno, APR, MAY,  JUN, JUL, AUG, SEP, OCT, NOV, DEC, JAN, FEB, MAR, finyear, plant);
-                if (i > 0)
+                if (op == "update")
                 {
-                    List<TOP18Data> data = TechnicalCommonService.GetRecordsTOP18(finyear, plant);
-                    ViewBag.records = data;
+                    int i = TechnicalCommonService.UpdateRecordsTOP18(Sno, Plant_cat, Uom, Pno, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC, JAN, FEB, MAR, finyear, plant);
+                    if (i > 0)
+                    {
+                        List<TOP18Data> data = TechnicalCommonService.GetRecordsTOP18(finyear, plant);
+                        ViewBag.records = data;
+                    }
+                    else if (i == -1)
+                    {
+                        CommonViewModel.errorMessage = "Sno,Fin Year and Plant must be unique";
+                        return Json(CommonViewModel);
+                    }
                 }
-                else if (i == -1)
+                else
                 {
-                    CommonViewModel.errorMessage = "Personel No already addedd";
-                    return Json(CommonViewModel);
+
+                    int i = TechnicalCommonService.SaveRecordsTOP18(Sno, Plant_cat, Uom, Pno, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC, JAN, FEB, MAR, finyear, plant);
+                    if (i > 0)
+                    {
+                        List<TOP18Data> data = TechnicalCommonService.GetRecordsTOP18(finyear, plant);
+                        ViewBag.records = data;
+                    }
+                    else if (i == -1)
+                    {
+                        CommonViewModel.errorMessage = "Sno,Fin Year and Plant must be unique ";
+                        return Json(CommonViewModel);
+                    }
                 }
 
             }
@@ -91,8 +148,6 @@ namespace IFFCO.TECHPROD.Web.Areas.M1.Controllers
                 return Json(CommonViewModel);
 
             }
-
-
 
 
             return PartialView("_partialTOP18");
