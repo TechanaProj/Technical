be8dfbcb02eaf9809539553198f07fc42cd89b86
Sumit Kumar <imsumitsingh_@outlook.com>
2022-03-13 15:19:50 +0530

    COMMIT

--- IFFCO.Web/wwwroot/js/TechnicalReporting.js
+++ IFFCO.Web/wwwroot/js/TechnicalReporting.js
@@ -27,19 +27,21 @@
             }
             $(".modalLoader").hide();
             if (response.report != null) {
-                var x = window.open('', '_blank');
-                x.document.write('<body></body>');
-                x.location.hash = response.selectedMenu;
-                var embedtag = x.document.createElement('embed');
-                embedtag.id = 'reportEmbed';
-                embedtag.src = response.report;
-                embedtag.style = "width:100%; height:100%;";
-                embedtag.alt = "pdf";
-                embedtag.title = "Report";
-                embedtag.type = "application/pdf";
-                embedtag.pluginspage = "http://www.adobe.com/products/acrobat/readstep2.html";
-                x.document.body.appendChild(embedtag);
-                x.document.title = response.selectedMenu;
+                var x = window.open(response.report, '_blank')
+                
+                //var x = window.open('', '_blank');
+                //x.document.write('<body></body>');
+                //x.location.hash = response.selectedMenu;
+                //var embedtag = x.document.createElement('embed');
+                //embedtag.id = 'reportEmbed';
+                //embedtag.src = response.report;
+                //embedtag.style = "width:100%; height:100%;";
+                //embedtag.alt = "pdf";
+                //embedtag.title = "Report";
+                //embedtag.type = "application/pdf";
+                //embedtag.pluginspage = "http://www.adobe.com/products/acrobat/readstep2.html";
+                //x.document.body.appendChild(embedtag);
+                //x.document.title = response.selectedMenu;
             }
 
 
