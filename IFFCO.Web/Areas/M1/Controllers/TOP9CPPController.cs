1484647170e7fe31945e33ca6516a0e165a26218
GrimJow <prodtchana@gmail.com>
2022-03-16 15:06:07 +0530

    TOP9 POWER Generation

---
+++ IFFCO.Web/Areas/M1/Controllers/TOP9CPPController.cs
@@ -0,0 +1,131 @@
+﻿using IFFCO.HRMS.Service;
+using IFFCO.TECHPROD.Web.CommonFunctions;
+using IFFCO.TECHPROD.Web.Controllers;
+using IFFCO.TECHPROD.Web.Models;
+using IFFCO.TECHPROD.Web.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace IFFCO.TECHPROD.Web.Areas.M1.Controllers
+{
+    [Area("M1")]
+    public class TOP9CPPController : BaseController<TOP9CPPViewModel>
+    {
+        private readonly ModelContext _context;
+        private readonly TechnicalCommonService TechnicalCommonService = null;
+        private readonly DropDownListBindWeb dropDownListBindWeb = null;
+        private readonly ReportRepositoryWithParameters reportRepository = null;
+        private readonly PrimaryKeyGen primaryKeyGen = null;
+        CommonException<TOP9CPPViewModel> commonException = null;
+
+        public TOP9CPPController(ModelContext context)
+        {
+            _context = context;
+            commonException = new CommonException<TOP9CPPViewModel>();
+            dropDownListBindWeb = new DropDownListBindWeb();
+            TechnicalCommonService = new TechnicalCommonService();
+            reportRepository = new ReportRepositoryWithParameters();
+            primaryKeyGen = new PrimaryKeyGen();
+        }
+        public IActionResult Index()
+        {
+            try
+            {
+                int EMP_ID = Convert.ToInt32(HttpContext.Session.GetInt32("EmpID"));
+                string moduleid = Convert.ToString(HttpContext.Session.GetString("ModuleID"));
+                string controller = this.ControllerContext.RouteData.Values["controller"].ToString();
+                List<CommonData> data = TechnicalCommonService.GetRecordsTOP9CPP(DateTime.Now.AddDays(-1), "SG");
+                ViewBag.reason = TechnicalCommonService.GetReason();
+                ViewBag.rights = TechnicalCommonService.GetScreenAccess(EMP_ID, controller, DateTime.Now.AddDays(-1));
+                ViewBag.records = data;
+            }
+            catch (Exception ex)
+            {
+
+                commonException.GetCommonExcepton(CommonViewModel, ex);
+                CommonViewModel.AreaName = this.ControllerContext.RouteData.Values["area"].ToString();
+                CommonViewModel.SelectedMenu = this.ControllerContext.RouteData.Values["controller"].ToString();
+                return Json(CommonViewModel);
+
+            }
+
+
+
+            return View(CommonViewModel);
+        }
+        public IActionResult Execute(string OperationType, string Plant, DateTime FromDate)
+        {
+            int EMP_ID = Convert.ToInt32(HttpContext.Session.GetInt32("EmpID"));
+            string moduleid = Convert.ToString(HttpContext.Session.GetString("ModuleID"));
+            string controller = this.ControllerContext.RouteData.Values["controller"].ToString();
+            try
+            {
+                switch (OperationType)
+                {
+                    case "query":
+                        List<CommonData> data = TechnicalCommonService.GetRecordsTOP9CPP(FromDate, "SG");
+                        ViewBag.reason = TechnicalCommonService.GetReason();
+                        ViewBag.records = data;
+                        ViewBag.rights = TechnicalCommonService.GetScreenAccess(EMP_ID, controller, FromDate);
+
+                        break;
+                    case "save":
+                        CommonViewModel.alert = "Data Saved";
+                        return Json(CommonViewModel);
+
+                    default:
+                        break;
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+
+                commonException.GetCommonExcepton(CommonViewModel, ex);
+                CommonViewModel.AreaName = this.ControllerContext.RouteData.Values["area"].ToString();
+                CommonViewModel.SelectedMenu = this.ControllerContext.RouteData.Values["controller"].ToString();
+                return Json(CommonViewModel);
+
+            }
+            return PartialView("_partialTOP9CPP");
+        }
+        public IActionResult PostData(string OperationType, string Plant, DateTime FromDate, string Input_Name, string Input_Value, string InputType)
+        {
+            try
+            {
+                switch (InputType)
+                {
+                    case "datetime-local":
+                        Input_Value = Convert.ToDateTime(Input_Value.Replace("T", " ")).ToString("MM/dd/yyyy HH:mm:ss");
+                        break;
+                    case "date":
+                        Input_Value = Convert.ToDateTime(Input_Value).Date();
+                        break;
+                    default:
+                        break;
+                }
+                int EMP_ID = Convert.ToInt32(HttpContext.Session.GetInt32("EmpID"));
+                string moduleid = Convert.ToString(HttpContext.Session.GetString("ModuleID"));
+                string controller = this.ControllerContext.RouteData.Values["controller"].ToString();
+
+
+                CommonViewModel.alert = TechnicalCommonService.PostRecordsTOP9CPP(FromDate, Plant, Input_Name, Input_Value, EMP_ID.ToString());
+
+            }
+            catch (Exception ex)
+            {
+
+                commonException.GetCommonExcepton(CommonViewModel, ex);
+                CommonViewModel.AreaName = this.ControllerContext.RouteData.Values["area"].ToString();
+                CommonViewModel.SelectedMenu = this.ControllerContext.RouteData.Values["controller"].ToString();
+                return Json(CommonViewModel);
+
+            }
+            return Json(CommonViewModel);
+        }
+
+    }
+}
---
+++ IFFCO.Web/ViewModels/TOPCPPViewModel.cs
@@ -0,0 +1,19 @@
+using IFFCO.HRMS.Shared.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IFFCO.TECHPROD.Web.ViewModels
+{
+    public class TOP9CPPViewModel : BaseModel
+    {
+        public string UCode { get; set; }
+        public string FromDate { get; set; }
+        public string ToDate { get; set; }
+        public string Shift { get; set; }
+        public string alert { get; set; }
+        public string errorMessage { get; set; }
+
+    }
+}
